1633cdf2e50887adb8b4fc079a6a59ba13bdbaa4
Replit user <>
2023-11-24 04:03:13 +0000

    change the link of those photos

--- seeds/index.js
+++ seeds/index.js
@@ -25,7 +25,7 @@ const seedDB = async () => {
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/random',
+      image: 'https://source.unsplash.com/collection/483251',
       description: 'Lorem ipsum dolor sit amet consectetur adioisici iure sed labore ipsam a cum nihil atque molestiae deserunt!',
       price
     });
