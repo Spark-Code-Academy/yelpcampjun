aac1e7c018fca38663bb0720800d578a4a68e222
Guangjun Ou <JUN@Guangjuns-MacBook-Pro.local>
2023-11-30 21:18:05 -0500

    create a review feature

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const Campground = require('./models/campground');
 
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-const { campgroundSchema } = require('./schemas')
+const { campgroundSchema, reviewSchema } = require('./schemas')
+
+const Review = require('./models/review')
 
 // Connection string to MongoDB Atlas
 
@@ -42,6 +44,16 @@ const validateCampground = (req, res, next) => {
       next();
   }
 };
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if(error) {
+    const msg = error.details.map(el => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+      next();
+  }
+}
 
 
 app.get('/', (req, res) => {
@@ -85,7 +97,7 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 
 
 app.get('/campgrounds/:id', catchAsync(async (req, res, next) => {
-  const campground = await Campground.findById(req.params.id);
+  const campground = await Campground.findById(req.params.id).populate('reviews');
   res.render('campgrounds/show', { campground })
 }));
 
@@ -105,6 +117,23 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res, next) => {
   await Campground.findByIdAndDelete(id);
   res.redirect(`/campgrounds`)
 }));
+
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`)
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+  const { id, reviewId } = req.params;
+  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/campgrounds/${id}`)
+}))
+
 
 app.all('*', (req, res, next) => {
   // res.send('404!!!')
